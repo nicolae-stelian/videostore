ab55290384a6b8f934f7a6844cfde7be38c48a6d
Astefanoaie Nicolae-Stelian <stelu26@gmail.com>
2013-12-29 21:26:14 +0100

    add test for 2 movies, for testing adding FrequentRenterPoints

--- test/VideoStore/CustomerTest.php
+++ test/VideoStore/CustomerTest.php
@@ -55,7 +55,7 @@ class CustomerTest extends \PHPUnit_Framework_TestCase
     public function testStatement_WithNoRentals()
     {
         $customer = new Customer('Customer Name');
-        $result = $customer->statement();
+        $result = $customer->txtStatement();
         $expected = "Rental Record for Customer Name\n";
         $expected .= "You owed 0\n";
         $expected .= "You earned 0 frequent renter points\n";
@@ -71,7 +71,7 @@ class CustomerTest extends \PHPUnit_Framework_TestCase
         $movie = new Movie('Movie', $movieType);
         $rental = new Rental($movie, $rentalDays);
         $customer->addRental($rental);
-        $result = $customer->statement();
+        $result = $customer->txtStatement();
 
         $expected = "Rental Record for Customer Name\n";
         $expected .= "\t%s\t%s\n";
@@ -84,6 +84,29 @@ class CustomerTest extends \PHPUnit_Framework_TestCase
             $expectedAmount,
             $expectedRenterPoints
         );
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testStatementWithTwoRentals()
+    {
+        $customer = new Customer('Customer Name');
+
+        $movie = new Movie('Regular 1', Movie::REGULAR);
+        $rental = new Rental($movie, 1);
+        $customer->addRental($rental);
+
+        $movie = new Movie('Regular 2', Movie::REGULAR);
+        $rental = new Rental($movie, 3);
+        $customer->addRental($rental);
+
+        $result = $customer->txtStatement();
+
+        $expected = "Rental Record for Customer Name\n";
+        $expected .= "\tRegular 1\t2\n";
+        $expected .= "\tRegular 2\t3.5\n";
+        $expected .= "You owed 5.5\n";
+        $expected .= "You earned 2 frequent renter points\n";
 
         $this->assertEquals($expected, $result);
     }
